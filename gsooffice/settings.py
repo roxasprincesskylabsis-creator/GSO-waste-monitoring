8c749e94717cea5dc8264b35392aaee4f2a8b295
Princess Kyla Roxas <kylaprincess219@gmail.com>
2026-09-23 00:23:33 +0800

    Configure Django environment variables

--- gsooffice/settings.py
+++ gsooffice/settings.py
@@ -17,18 +17,18 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@*&s$#h^rb9_z5l-f@cx6co^syi_0*n+c*_-5w3t9l7usu$@=x'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
